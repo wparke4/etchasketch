9e16d9a90af3495e71ad1028704c9c3d498571dc
wparke4 <willparke4@gmail.com>
2021-10-02 13:06:50 -0700

    user can now select between black, random, or eraser for background color

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,30 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.cellContainer');
+const eraserButton = document.getElementById('eraserButton');
+const blackButton = document.getElementById('blackButton');
+const randButton = document.getElementById('randButton');
+const resetButton = document.getElementById('resetButton');
+
+let r = 0;
+let g = 0;
+let b = 0;
+
+eraserButton.addEventListener('click', () => {
+    r = 255;
+    g = 255;
+    b = 255;
+})
+
+blackButton.addEventListener('click', () => {
+    r = 0;
+    g = 0;
+    b = 0;
+})
+
+randButton.addEventListener('click', () => {
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+})
 
 function makeRows(rows, cols) {
     //set the css variables (--grid-rows) equal to the argument passed into this function
@@ -10,13 +36,14 @@ function makeRows(rows, cols) {
         let cell = document.createElement('div');
         //making the new div created (named cell) to be a child of the container div 
         //also applying the grid-item class and therefore all of its styling to the cell divs
-        container.appendChild(cell).className = "grid-item";
+        container.appendChild(cell);
+        cell.classList.add('grid-item');
+        //when cell is hovered, adding class of hovering
         cell.addEventListener('mouseover', () => {
-            cell.classList.add('hovering');
+            cell.style.backgroundColor = "rgb("+ r +","+ g +","+ b +")";
         })
     };
 };
-
 
 /* To-Do: 
     Allow user to select amount of cells to make
